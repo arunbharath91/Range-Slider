37ea7f78201685b16db52da52c0e2aeb5dc9a357
Arun Bharath <arunbharath91@gmail.com>
2020-05-02 21:14:14 +0530

    update

---
+++ src/app/range-slider.ts
@@ -0,0 +1,134 @@
+interface IOptions {
+  min?: number;
+  max?: number;
+  tooltip?: boolean;
+  step?: number;
+  value?: number[];
+  onChange?:Function
+}
+
+const defaultOptions:IOptions = {
+  min: 0,
+  max: 100,
+  tooltip: true,
+  step: 0,
+  value: [0, 100]
+}
+ class RangeSlider extends HTMLElement {
+  private options!: IOptions;
+  private inputLeft!:HTMLElement;
+  private inputRight!:HTMLElement;
+
+  private thumbLeft!:HTMLElement;
+  private thumbRight!:HTMLElement;
+  private range!:HTMLElement;
+  private tooltips!: NodeListOf<Element>;
+  constructor() {
+    super();
+    this.config();
+  }
+
+  config() {
+    const getAllAttr:any = {};
+    Array.from(this.attributes).forEach((item) => {
+      if(/\S/.test(item.value)) {
+        getAllAttr[item.name] = (item.name !== 'onchange') ? eval(item.value) : item.value;
+      }
+    });
+    this.options = {...defaultOptions, ...getAllAttr};
+    this.setDefaultAttr();
+  }
+
+  setDefaultAttr(){
+    this.setAttribute('min', (this.options.min as any).toString());
+    this.setAttribute('max', (this.options.max as any).toString());
+    this.setAttribute('value', `[${this.options.value}]`);
+    this.projectTemplate();
+  }
+
+  projectTemplate() {
+  const inputs = `
+  <div class="input-range">
+  <input type="range" class="handleBarRef1" name="points1" min="${this.options.min}" max="${this.options.max}" value="${(this.options.value as any)[0]}" />
+  <input type="range" class="handleBarRef2" name="points2" min="${this.options.min}" max="${this.options.max}" value="${(this.options.value as any)[1]}" />
+  </div>
+  <div class="slider">
+  <div class="track"></div>
+  <div class="range"></div>
+  <div class="thumb left">
+  ${(this.options.tooltip) ? '<tooltip></tooltip>' : ''}
+  </div>
+  <div class="thumb right">
+  ${(this.options.tooltip) ? '<tooltip></tooltip>' : ''}
+  </div>
+  </div>
+  `;
+  this.insertAdjacentHTML('beforeend', inputs);
+  if(this.options.step) {
+  (this.querySelector('.track') as HTMLElement).style.cssText = `
+  background-size: ${this.options.step}% 2px;
+  background-position: center;
+  background-image: repeating-linear-gradient(to right, rgb(98, 0, 238), rgb(202, 172, 244) 2px, transparent 0, transparent);`
+  }
+  this.initRange();
+  }
+
+  initRange() {
+  this.inputLeft = (this.querySelector('.handleBarRef1') as HTMLElement);
+  this.inputRight = (this.querySelector('.handleBarRef2') as HTMLElement);
+
+  this.thumbLeft = (this.querySelector('.slider > .thumb.left') as HTMLElement);
+  this.thumbRight = (this.querySelector('.slider > .thumb.right') as HTMLElement);
+  this.range = (this.querySelector('.slider > .range') as HTMLElement);
+  this.tooltips = this.querySelectorAll('tooltip');
+
+  this.inputLeft.addEventListener('input', (e) => this.setLeftValue(e.target));
+  this.setLeftValue(this.inputLeft);
+
+  this.inputRight.addEventListener('input', (e) => this.setRightValue(e.target));
+  this.setRightValue(this.inputRight);
+
+  this.inputLeft.addEventListener('change', () => {
+    this.setAttribute('value', `[${this.options.value}]`)
+    if(this.options.onChange) this.options.onChange.call(this);
+  });
+  this.inputRight.addEventListener('change', () => {
+    this.setAttribute('value', `[${this.options.value}]`)
+    if(this.options.onChange) this.options.onChange.call(this);
+  });
+  }
+
+  setLeftValue(elem: any) {
+  let min = parseInt(elem.min);
+  let max = parseInt(elem.max);
+  let rightValue = Number(this.inputRight.getAttribute('value'));
+  elem.setAttribute('value', elem.value);
+  elem.value = Math.min(elem.value, rightValue-1);
+
+  let percent = ((elem.value - min)/(max-min)) * 100;
+
+  this.thumbLeft.style.left = `${percent}%`;
+  this.range.style.left = `${percent}%`;
+  this.tooltips[0].innerHTML = elem.value;
+  this.options.value = [Number(elem.value), rightValue];
+  }
+
+  setRightValue(elem: any) {
+  let min = parseInt(elem.min);
+  let max = parseInt(elem.max);
+  let leftValue = Number(this.inputLeft.getAttribute('value'));
+  elem.setAttribute('value', elem.value);
+  elem.value = Math.max(elem.value, leftValue+1);
+
+
+  let percent = ((elem.value - min)/(max-min)) * 100;
+
+  this.thumbRight.style.right = `${100-percent}%`;
+  this.range.style.right = `${100-percent}%`;
+  this.tooltips[1].innerHTML = elem.value;
+  this.options.value = [leftValue, Number(elem.value)];
+  }
+
+}
+
+window.customElements.define("app-range", RangeSlider);
